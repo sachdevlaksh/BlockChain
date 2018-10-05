e5f95e5ebd4ac7451d7e0a3dd45dddffd7dc862e
chandrakala <37008283+chandu2416@users.noreply.github.com>
2018-10-05 16:31:58 +0530

    Update kapy_api.js

--- server/routes/kapy_api.js
+++ server/routes/kapy_api.js
@@ -91,7 +91,7 @@ router.post('/api/addFarmerRecordKapy', (req, res) => {
 
 console.log("Farmer request body :" +JSON.stringify(FarmerReq));
 
-requestify.request('http://ec2-52-90-144-179.compute-1.amazonaws.com:3000/api/Registration', {
+requestify.request('http://ec2-52-90-144-179.compute-1.amazonaws.com:3000/api/LandRecord', {
 method: 'POST',
 body: FarmerReq ,
 dataType: 'json' 
